74919faedcfe6c60ed704cb4b9f7093092ebbdef
Simon_Mac_Aree <smacr25@gmail.com>
2018-07-07 12:19:52 +0000

    Testing and index changes

---
+++ picture_test.py
@@ -0,0 +1,8 @@
+questions_answers = {
+    "picture one": "treasure island",
+    "picture two": "batman",
+}
+
+for answer in questions_answers.key("treasure island"):
+        if answer == key("treasure island")
+            print("correct")
