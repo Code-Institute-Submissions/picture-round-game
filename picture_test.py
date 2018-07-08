c52fd4522cf619a19461b0f40ea6b6a58e9514e9
Simon_Mac_Aree <smacr25@gmail.com>
2018-07-08 12:44:02 +0000

    debugging test and index

--- picture_test.py
+++ picture_test.py
@@ -1,8 +1,22 @@
-questions_answers = {
-    "picture one": "treasure island",
-    "picture two": "batman",
-}
+def round_one(questions_and_answers):
+    questions_and_answers ={
+        "picture one": "treasure island",
+        "picture two": "batman",
+    }
+    for answer in questions_and_answers.keys():
+        questions_and_answers[answer]
+    if answer == "treasure island":
+        print("Correct")
+    else:
+        return("incorrect")
+    return questions_and_answers   
+    
+assert round_one("treasure island") == 1, "Correct"
+assert round_one("batman") == 0, "incorrect" 
 
-for answer in questions_answers.key("treasure island"):
-        if answer == key("treasure island")
-            print("correct")
+print("All the tests passed") 
+
+#def round_one(picture):
+#    return("False")
+#assert round_one([]) == False, "No picture" 
+#print("All tests passed")
