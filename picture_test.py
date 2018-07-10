1a48d58437f1f361b53cb85eaaa80aa5c5cf8760
Simon_Mac_Aree <smacr25@gmail.com>
2018-07-10 20:35:56 +0000

    Adding user input and save username

--- picture_test.py
+++ picture_test.py
@@ -1,22 +1,116 @@
-def round_one(questions_and_answers):
-    questions_and_answers ={
-        "picture one": "treasure island",
-        "picture two": "batman",
-    }
-    for answer in questions_and_answers.keys():
-        questions_and_answers[answer]
-    if answer == "treasure island":
-        print("Correct")
-    else:
-        return("incorrect")
-    return questions_and_answers   
+import os
+import json
+
+SCRIPT_PATH = os.path.join(os.getcwd(), os.path.dirname(__file__))
+USER_FILE_NAME = "user_data.json"
+QUESTIONS_FILE_NAME = "questions_answer.json" #Trying to add question json file
+
+user_data = []
+
+
+
+def load_user_data(file_name):
+    """
+    Load Dictionary file
+    """
+    if not file_name.startswith('/'):
+        # if not absolute, then make path relative to our location:
+        file_name = os.path.join(SCRIPT_PATH, file_name)
+
+    with open(file_name, "r") as f:
+        user_data = f.read()
+        user_data = json.loads(user_data)
+
+    return user_data
+
+
     
-assert round_one("treasure island") == 1, "Correct"
-assert round_one("batman") == 0, "incorrect" 
+def write_user_data(file_name, user_data):
+    """
+    Load Dictionary file
+    """
+    if not file_name.startswith('/'):
+        # if not absolute, then make path relative to our location:
+        file_name = os.path.join(SCRIPT_PATH, file_name)
 
-print("All the tests passed") 
+    
+    with open(file_name, "w") as f:
+        f.write(json.dumps(user_data))
+        #f.read()
+        #user_data = json.loads(user_data)
+        #print("user_data", user_data)
+    
+    
+def add_user_data(user, user_data):
+    load_user_data(USER_FILE_NAME)
+    user_data.append(user)
+    write_user_data(USER_FILE_NAME, user_data)
 
-#def round_one(picture):
-#    return("False")
-#assert round_one([]) == False, "No picture" 
-#print("All tests passed")
+#user_data = load_user_data('user_data.json')
+#write_user_data('user_data.json', user_data)
+
+print(user_data)
+
+user_name = input('Pick your user name ')
+print('Hello', user_name)
+
+user = user_name
+
+
+add_user_data(user, user_data)
+
+print(user_data)
+
+questions = [
+    {
+        "title": "Question One",
+        "photos": [
+            "photo1.jpg",
+            "photo2.jpg"
+        ],
+        "answer": "treasure island",
+    },
+    {
+        "title": "Question Two",
+        "photos": [
+            "photo3.jpg",
+            "photo4.jpg"
+        ],
+        "answer": "treasure island",
+    },
+]
+
+
+data_answers = { 
+    "round_one": "treasure island",
+    "round_two": "batman",
+}
+
+def get_question(question_number):
+    
+    question = questions[question_number]
+    
+    return question['title']
+
+def check_answer(question_number, user_answer):
+    
+    user_answer = user_answer.lower().strip()
+    
+    question = questions[question_number]
+    
+    result = question["answer"] == user_answer
+    
+    return result
+
+    
+
+print("get_question:", get_question(0))#question one in array
+print("get_question:", get_question(1))#question two
+
+print("check_answer:", check_answer(0, "test")) #checking function works
+print("check_answer:", check_answer(0, "Treasure Island")) #checking answer should return True  
+print("check_answer:", check_answer(0, " TReAsuRe Island  "))#checking answer should return True  
+print("check_answer:", check_answer(0, " 123456  "))#checking answer should return False  
+print("check_answer:", check_answer(0, " £$%^&*()  "))#checking answer should return False 
+
+print("all tests passed")
